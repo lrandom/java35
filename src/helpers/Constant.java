a1b4aa14643e217966dec7ccbad459c8858066bb
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2024-03-11 21:11:28 +0700

    session 3

--- src/helpers/Constant.java
+++ src/helpers/Constant.java
@@ -2,10 +2,14 @@ package helpers;
 
 import java.util.Scanner;
 
-public class Constant {
-    public static String labelInputData = "Vui long nhap data";
+final public class Constant {
+    public static final String labelInputData = "Vui long nhap data";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+    }
+
+    final void abc() {
+
     }
 }
